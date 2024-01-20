63b0d2c2fc770aa9591eb8d24b03c76a4f879ad4
Noah Muhammad <noahxmuhammad@gmail.com>
2024-01-20 03:15:41 -0500

    added mobile scaling for header final

--- src/Header.js
+++ src/Header.js
@@ -28,7 +28,7 @@ function Header() {
               <span className="header__option2">Sign In</span>
             </div>
           </Link>
-          <Link to="/orders" className="header__link">
+          <Link to="/" className="header__link">
             <div className="header__option">
               <span className="header__option1">Returns</span>
               <span className="header__option2">& Orders</span>
