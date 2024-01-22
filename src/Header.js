36b379f20dfcefae2fb0dfc682a097bc3ba8765f
Noah Muhammad <noahxmuhammad@gmail.com>
2024-01-22 07:05:32 -0500

    added sign out

--- src/Header.js
+++ src/Header.js
@@ -4,9 +4,16 @@ import SearchIcon from '@material-ui/icons/Search';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import { useStateValue } from "./StateProvider";
 import './Header.css';
+import { auth } from './firebase';
 
 function Header() {
   const [{ basket, user }, dispatch] = useStateValue();
+
+  const handleAuthenticaton = () => {
+		if (user){
+			auth.signOut();
+		}
+	};
 
   return (
     <header className="header">
@@ -18,11 +25,11 @@ function Header() {
         <SearchIcon className="header__searchIcon" />
       </div>
       <nav className="header__nav">
-        <Link to="/login" className="header__link">
+        <Link to= {!user && "/login" }className="header__link" onClick={handleAuthenticaton}>
           <span style={{ fontSize: '12px', fontWeight: 750 }}>
-            Hello {user ? user.displayName : 'Guest'}
+            Hello {user ? user.email : 'Guest'}
           </span><br />
-          <span style={{ fontSize: '16px', fontWeight: 800 }}>Sign In</span>
+          <span  style={{ fontSize: '16px', fontWeight: 800 }}>{user ? "Sign Out" : "Sign In"}</span>
         </Link>
         <Link to="/login" className="header__link">
           <span style={{ fontSize: '12px', fontWeight: 750 }}>Returns</span><br />
