d0ca6e0aa767f92c2ad94e0811f61c049f5b926c
Noah Muhammad <noahxmuhammad@gmail.com>
2024-01-24 10:17:13 -0500

    hard quoted data version

--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './Home.css';
 import Product from './Product';
 import Slider from 'react-slick'; // Import Slider component
 import "slick-carousel/slick/slick.css"; // Default styling
 import "slick-carousel/slick/slick-theme.css"; // Default theme
-import axios from 'axios';
 
 function Home() {
-    const [productData, setProductData] = useState([]);
-
-    useEffect(() => {
-        axios.get('/api/products') // Replace with your backend API endpoint
-            .then((response) => {
-                const products = response.data;
-                setProductData(products);
-            })
-            .catch((error) => {
-                console.error('Error fetching data:', error);
-            });
-    }, []);
-
     const slides = [
+        
         "/1120679.jpg",
         "/intro-1611252336.jpg",
         "/amazon-prime-video-novita-catalogo-serie-tv-film-dicembre-2022 (1).jpg"
+        
     ];
 
     const settings = {
@@ -49,19 +37,116 @@ function Home() {
             </div>
 
             <div className="home__productsrow1">
-                {productData.map((product) => (
-                    <Product
-                        key={product.id}
-                        id={product.id}
-                        title={product.title}
-                        price={product.price}
-                        rating={product.rating}
-                        image={product.image}
-                    />
-                ))}
+                <Product
+                    id="12345"
+                    title="Amazon Echo | Smart speaker in Grey"
+                    price={12.99}
+                    rating={4}
+                    image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
+                />
+                 <Product
+                    id="9101112"
+                    title="Apple AirPods Max Wireless Headphones"
+                    price={9.99}
+                    rating={3}
+                    image="https://m.media-amazon.com/images/I/81IfN-Rw8uL._AC_SX522_.jpg"
+                />
+            </div>
+
+            <div className="home__productsrow2">
+                <Product
+                    id="56789"
+                    title="Fitbit Pai Chai Watch for Fitness Tracking"
+                    price={18.99}
+                    rating={5}
+                    image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
+                /> 
+                <Product
+                    id="13141516"
+                    title="Coding for Dummies Java Script"
+                    price={8.00}
+                    rating={2}
+                    image="https://m.media-amazon.com/images/I/51QAlEX+W1L._SY342_.jpg"
+                />
+                <Product
+                    id="1718192021"
+                    title="New Apple iPad Pro (12.9-inch, 128GB)"
+                    price={18.49}
+                    rating={3}
+                    image="https://pisces.bbystatic.com/image2/BestBuy_US/images/products/5499/5499200cv14d.jpg;maxHeight=2000;maxWidth=2000"
+                />
             </div>
         </div>
     );
 }
 
 export default Home;
+
+// -----------------------------------------------------
+// import React, { useEffect, useState } from 'react';
+// import './Home.css';
+// import Product from './Product';
+// import Slider from 'react-slick'; // Import Slider component
+// import "slick-carousel/slick/slick.css"; // Default styling
+// import "slick-carousel/slick/slick-theme.css"; // Default theme
+// import axios from 'axios';
+
+// function Home() {
+//     const [productData, setProductData] = useState([]);
+
+//     useEffect(() => {
+//         axios.get('/api/products') // Replace with your backend API endpoint
+//             .then((response) => {
+//                 const products = response.data;
+//                 setProductData(products);
+//             })
+//             .catch((error) => {
+//                 console.error('Error fetching data:', error);
+//             });
+//     }, []);
+
+//     const slides = [
+//         "/1120679.jpg",
+//         "/intro-1611252336.jpg",
+//         "/amazon-prime-video-novita-catalogo-serie-tv-film-dicembre-2022 (1).jpg"
+//     ];
+
+//     const settings = {
+//         dots: false,
+//         infinite: true,
+//         speed: 500,
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         autoplay: true,
+//         autoplaySpeed: 3000,
+//     };
+
+//     return (
+//         <div className="home">
+//             <div className="home__carousel">
+//                 <Slider className="slider-container" {...settings}>
+//                     {slides.map((slide, index) => (
+//                         <div key={index}>
+//                             <img src={slide} alt={`Slide ${index}`} />
+//                         </div>
+//                     ))}
+//                 </Slider>
+//             </div>
+
+//             <div className="home__productsrow1">
+//                 {productData.map((product) => (
+//                     <Product
+//                         key={product.id}
+//                         id={product.id}
+//                         title={product.title}
+//                         price={product.price}
+//                         rating={product.rating}
+//                         image={product.image}
+//                     />
+//                 ))}
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default Home;
--- src/Product.js
+++ src/Product.js
@@ -1,12 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './Product.css';
 import { useStateValue } from './StateProvider';
-import axios from 'axios';
-
 
 function Product({ id, title, image, price, rating }) {
   const [{ basket }, dispatch] = useStateValue();
-  const [productData, setProductData] = useState([]);
 
   const addToBasket = () => {
     // Dispatch the item into the data layer
@@ -21,18 +18,6 @@ function Product({ id, title, image, price, rating }) {
       },
     });
   };
-
-  useEffect(() => {
-    axios.get('/api/products') // Replace with your backend API endpoint
-      .then((response) => {
-        // Handle the API response here
-        const products = response.data;
-        setProductData(products);
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-      });
-  }, []);
 
   return (
     <div className="product">
@@ -57,3 +42,64 @@ function Product({ id, title, image, price, rating }) {
 }
 
 export default Product;
+
+// -----------------------------------------------------
+// import React, { useEffect, useState } from 'react';
+// import './Product.css';
+// import { useStateValue } from './StateProvider';
+// import axios from 'axios';
+
+
+// function Product({ id, title, image, price, rating }) {
+//   const [{ basket }, dispatch] = useStateValue();
+//   const [productData, setProductData] = useState([]);
+
+//   const addToBasket = () => {
+//     // Dispatch the item into the data layer
+//     dispatch({
+//       type: "ADD_TO_BASKET",
+//       item: {
+//         id: id,
+//         title: title,
+//         image: image,
+//         price: price,
+//         rating: rating,
+//       },
+//     });
+//   };
+
+//   useEffect(() => {
+//     axios.get('/api/products') // Replace with your backend API endpoint
+//       .then((response) => {
+//         // Handle the API response here
+//         const products = response.data;
+//         setProductData(products);
+//       })
+//       .catch((error) => {
+//         console.error('Error fetching data:', error);
+//       });
+//   }, []);
+
+//   return (
+//     <div className="product">
+//       <div className="product__info">
+//         <p>{title}</p>
+//         <p className="product__price">
+//           <small>$</small>
+//           <strong>{price}</strong>
+//         </p>
+//         <div className="product__rating">
+//           {Array(rating)
+//             .fill()
+//             .map((_, i) => (
+//               <p key={i}>⭐</p>
+//             ))}
+//         </div>
+//       </div>
+//       <img className="product__image" src={image} alt={title} />
+//       <button onClick={addToBasket} className="product__button">Add to Cart</button>
+//     </div>
+//   );
+// }
+
+// export default Product;
